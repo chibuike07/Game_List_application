af16ab9c3e238e84ecf5087c6d8e867e12451c95
hobbyluv <princewillchime43@gmail.com>
2021-07-26 00:09:34 +0100

    some changes

--- src/helper/CustomFetch.js
+++ src/helper/CustomFetch.js
@@ -1,13 +1,15 @@
+import axios from "axios";
 import { errorToastify } from "../common/react_toastify/toastify";
-import { AuthAxios } from "./CustomAxios";
+// import { AuthAxios } from "./CustomAxios";
 
 import { switchData } from "./switchData";
 
 export const fetchRequest = async ({ url, storage, dispatch }) => {
-  await AuthAxios.get(url, {
-    "Content-Type": "application/json",
-    withCredentials: true,
-  })
+  await axios
+    .get(url, {
+      "Content-Type": "application/json",
+      withCredentials: true,
+    })
     .then((res) => {
       res.data && switchData({ storage, dispatch, payload: res.data });
     })
